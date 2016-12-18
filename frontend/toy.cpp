e2227b3e3193309022e25e332bd9fc043c707be3
707 <707472783@qq.com>
2016-12-19 00:09:14 +0800

    update toy.cpp

--- frontend/toy.cpp
+++ frontend/toy.cpp
@@ -1,4 +1,10 @@
 #include <string>
+#include <vector>
+
+/**
+ * 词法分析
+ */
+
 enum Token_type {
     EOF_TOKEN = 0,
     NUMERIC_TOKEN,
@@ -55,3 +61,56 @@ static int get_token() {
     LastChar = fgetc(file);
     return ThisChar;
 }
+
+/**
+ * 抽象语法树
+ */
+
+class BaseAST {
+public:
+    virtual ~BaseAST();
+};
+
+class VariableAST : public BaseAST {
+    std::string Var_Name;
+public:
+    VariableAST (std::string &name) : Var_Name(name) {}
+};
+
+class NumericAST : public BaseAST {
+    int numeric_val;
+public:
+    NumericAST (int val) : numeric_val(val) {}
+};
+
+class BinaryAST : public BaseAST {
+    std::string Bin_Operator;
+    BaseAST *LHS, *RHS;
+public:
+    BinaryAST (std::string op, BaseAST *lhs, BaseAST *rhs) :
+    Bin_Operator(op), LHS(lhs), RHS(rhs) {}
+};
+
+class FunctionDeclAST {
+    std::string Func_Name;
+    std::vector<std::string> Arguments;
+public:
+    FunctionDeclAST (const std::string &name, const std::vector<std::string> &args) :
+    Func_Name(name), Arguments(args) {}
+};
+
+class FunctionDefnAST {
+    FunctionDeclAST *Func_Decl;
+    BaseAST *Body;
+public:
+    FunctionDefnAST (FunctionDeclAST *proto, BaseAST *body) :
+    Func_Decl(proto), Body(body) {}
+};
+
+class FunctionCallAST : public BaseAST {
+    std::string Function_Callee;
+    std::vector<BaseAST*> Function_Arguments;
+public:
+    FunctionCallAST (const std::string &callee, std::vector<BaseAST*> &args) :
+    Function_Callee(callee), Function_Arguments(args) {}
+};
